7836a596a6a8b8be39a769e940577821838bf44d
bekirgeris <bekirgeris@marun.edu.tr>
2021-12-07 14:47:44 +0300

    +retrofit call

--- app/src/main/java/com/begers/retrofitkotlin/view/MainActivity.kt
+++ app/src/main/java/com/begers/retrofitkotlin/view/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.begers.retrofitkotlin.R
 import com.begers.retrofitkotlin.model.CryptoModel
+import com.begers.retrofitkotlin.service.CryptoAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +19,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+    }
+
+    fun loadData(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(CryptoAPI::class.java)
+        val call = service.getAll()
+
+        call.enqueue(object: Callback<List<CryptoModel>> {
+            override fun onResponse(call: Call<List<CryptoModel>>, response: Response<List<CryptoModel>>) {
+                if (response.isSuccessful){
+                    response.body()?.let {
+                        cryptoModels = ArrayList(it)
+
+                        for (cryptoModel in cryptoModels){
+                            println(cryptoModel.currency)
+                            println(cryptoModel.price)
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+
+            }
+        })
     }
 }
